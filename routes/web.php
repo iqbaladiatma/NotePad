a772cb3f53c96c36bf8726df9a762070406fb001
iqbaladiatma <gazamuhammadadiatmav@gmail.com>
2025-05-23 08:30:01 +0700

    ini udah setengah jadi

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\NoteController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [NoteController::class, 'index'])->name('dashboard');
+    Route::resource('notes', NoteController::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
